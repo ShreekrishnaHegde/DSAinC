b0059d155504bec2814aa30f1aa7ab9cdab016d6
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-11-13 04:19:11 +0530

    msg

---
+++ 13_11_2024/LinearQueue.c
@@ -0,0 +1,6 @@
+//Program to show operations on linear queue.
+#include <stdio.h>
+int queue[10];
+void main(){
+    
+}
---
+++ sudoku.c
@@ -0,0 +1,53 @@
+//Backtracking for sudoku
+#include <stdio.h>
+#include <stdbool.h>
+#define m 9
+#define n 9
+int board[9][9] ={
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+    };
+void display();
+bool solve();
+void main(){
+    
+    if(solve(board))
+        display(board);
+    else
+        printf("\nCannot Solve sudoku");
+}
+bool solve(){
+    //To keep track of unknown elements
+    int row=-1;
+    int col=-1;
+    bool flag=true;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(board[i][j]==0){
+                row=i;
+                col=j;
+                flag=false;
+                break;
+            }
+        }
+        if(!flag)
+            break;
+    }
+    //Sudoku is solved
+    if(flag==true)
+        return true;
+}
+void display(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+            printf("%d ",board[i][j]);
+        printf("");
+    }
+}
