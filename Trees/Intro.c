5f70db9f9be58ad4f4350d5d8f9c7155230a3ef8
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-01-02 17:49:55 +0530

    Traversal

---
+++ Trees/Intro.c
@@ -0,0 +1,22 @@
+/*
+->Parent Node: The node which is an immediate predecessor of a node is called the parent node of that node.
+->Root Node: The topmost node of a tree or the node which does not have any parent node is called the root node.  
+->A non-empty tree must contain exactly one root node and exactly one path from the root to all other nodes of the tree.
+->Leaf Node or External Node: The nodes which do not have any child nodes are called leaf nodes.
+->Ancestor of a Node: Any predecessor nodes on the path of the root to that node are called Ancestors of that node.
+->Descendant: A node x is a descendant of another node y if and only if y is an ancestor of x.
+->Sibling: Children of the same parent node are called siblings. {D,E} are called siblings.
+->Level of a node: The count of edges on the path from the root node to that node. The root node has level 0.
+->Internal node: A node with at least one child is called Internal Node.
+->Neighbour of a Node: Parent or child nodes of that node are called neighbors of that node.
+->Subtree: Any node of the tree along with its descendant.
+
+
+
+--------------------------------------------------Properties--------------------------------------------
+->Recursive Data Strucure
+->Number of edges
+->Height of a node
+->Depth of a node
+
+*/
---
+++ Trees/TraversingBT.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{
+    int data;
+    struct Node *left,*right;
+}Node;
+
+Node* create(int data){
+    Node* newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=data;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
+void printInOrder(Node* node);
+void printPreOrder(Node* node);
+void printPostOrder(Node* node);
+
+void main(){
+
+    Node* root=create(1);
+    root->left=create(2);
+    root->right=create(3);
+    root->left->left=create(4);
+    root->left->right=create(5);
+    root->right->right=create(6);
+
+    printf("\nInorder Traversal: ");
+    printInOrder(root);
+    printf("\nPreOrder Traversal: ");
+    printPreOrder(root);
+}
+
+/*
+-------------------------Algorithm for inorder Traversal--------------------
+Inorder(root):
+If root is NULL, then return
+Inorder (root -> left)
+Process root (For example, print root’s data)
+Inorder (root -> right)
+*/
+void printInOrder(Node* node){
+    //Base Condition
+    if(node==NULL)
+        return;
+    //Print Left Sub tree
+    printInOrder(node->left);
+    //Print Node
+    printf("%d ",node->data);
+    //Print Right Sub tree
+    printInOrder(node->right);
+}
+/*
+-----------------------------algorithm for PreOrder Traversal-----------------------------
+If root is NULL then return
+Process root (For example, print root’s data)
+Preorder (root -> left)
+Preorder (root -> right)
+*/
+void printPreOrder(Node* node){
+    if(node==NULL)
+        return;
+    printf(" %d",node->data);
+    printPreOrder(node->left);
+    printPreOrder(node->right);
+}
+/*
+-----------------------------------Algorithm for PostOrder Traversal------------------------
+Traverse left subtree
+Traverse the right subtree
+Visit the root and print the data.
+*/
+void printPostOrder(Node* node){
+    if(node==NULL)
+        return;
+    printPostOrder(node->left);
+    printPostOrder(node->right);
+    printf(" %d",node->data);
+}
