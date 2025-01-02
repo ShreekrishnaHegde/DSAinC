9cda8a0378aa7a00148ea076c836044c580b0c7b
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-01-02 15:09:28 +0530

    Added basic operations without algo

--- LinkedList/Implementation/SinglyLL.c
+++ LinkedList/Implementation/SinglyLL.c
@@ -8,6 +8,27 @@ typedef struct Node {
     struct Node* next;
 }Node;
 
+//Operations on Singly Linked List
+Node* createNode(int data);
+void insertAtFirst(Node** head);
+void deleteFromFirst(Node** head);
+void insertAtLast(Node** head);
+void deleteLast(Node** head);
+void insertAtPos(Node **head);
+void deleteAtPos(Node** head);
+void print(Node* head);
+void size(Node* head);
+// Driver Code
+int main() {
+    struct Node* head = NULL;
+    insertAtFirst(&head);
+    insertAtLast(&head);
+    print(head);
+    insertAtPos(&head);
+    print(head);
+    return 0;
+}
+
 // Function to create a new node 
 Node* createNode(int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
@@ -15,7 +36,6 @@ Node* createNode(int data) {
     newNode->next = NULL;
     return newNode;
 }
-
 // Function to insert a new element at the beginning of the singly linked list
 void insertAtFirst(Node** head) {
     int data;
@@ -54,12 +74,70 @@ void insertAtLast(Node** head){
 }
 //Function to delete the last node of the linked list.
 void deleteLast(Node** head){
+    //If list is empty
+    if(*head==NULL){
+        printf("\nList is empty!");
+        return;
+    }
     Node *temp=*head;
+    //If list has only 1 node
+    if(temp->next==NULL){
+        free(temp);
+        *head=NULL;
+        return;
+    }
+    //If list has more than 1 node
     while(temp->next->next!=NULL){
         temp=temp->next;
     }
     free(temp->next);
-    
+    temp->next=NULL;
+}
+//Function to insert node at given position in linked list
+void insertAtPos(Node **head){
+    int data,pos;   
+    Node *temp=*head,*newNode=NULL;
+    printf("\nEnter the position: ");
+    scanf("%d",&pos);
+    //If position is 0, then call insertFirst function
+    if(pos==0){
+        insertAtFirst(head);
+        return;
+    }
+    printf("\nEnter the value: ");
+    scanf("\n%d",&data);
+    newNode=createNode(data);
+    for(int i=0;i<pos-1 && temp!=NULL ;i++){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;    
+}
+//Function to delete a node at given position
+void deleteAtPos(Node** head){
+    int pos,data;
+    Node *temp=*head,*deleted;
+    printf("\nEnter the position: ");
+    scanf("%d",&pos);
+    if(*head==NULL){
+        printf("\nList is empty");
+        return;
+    }
+    if(pos==0){
+        deleteFromFirst(head);
+        return;
+    }
+    printf("\nEnter the value: ");
+    scanf("%d",&data);
+    for(int i=0;i<pos && temp!=NULL;i++)
+        temp=temp->next;
+    if(temp==NULL || temp->next==NULL){
+        printf("\nPosition out of range");
+        return;
+    }
+    deleted=temp->next;
+    temp->next=temp->next->next;
+    free(deleted);    
 }
 // Function to print the LinkedList
 void print(Node* head) {
@@ -69,20 +147,16 @@ void print(Node* head) {
         temp = temp->next;
     }
     printf("NULL\n");
+    size(head);
 }
 
-// Driver Code
-int main() {
-    struct Node* head = NULL;
-    insertAtFirst(&head);
-    insertAtFirst(&head);
-    insertAtFirst(&head);
-    print(head); 
-    deleteFromFirst(&head);
-    print(head);
-    insertAtLast(&head);
-    insertAtLast(&head);
-    print(head);
-
-    return 0;
+void size(Node* head){
+    Node* temp=head;
+    int count=0;
+    while (temp!=NULL){
+        temp=temp->next;
+        ++count;
+    }
+    printf("\nSize of the list is: %d\n",count);
+    
 }
