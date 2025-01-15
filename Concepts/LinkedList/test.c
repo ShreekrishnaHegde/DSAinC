1edccd27d1ecdc2bf3fa6077a6d137e8ed0d0879
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-01-15 21:16:01 +0530

    Completed DM

--- Concepts/LinkedList/test.c
+++ Concepts/LinkedList/test.c
@@ -2,14 +2,94 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define MAX 100
+bool isReflexive(int*,int,int*,int);
+bool isIrreflexive(int*,int,int*,int);
+bool isSymmetric(int*,int);
+bool isAntisymmetric(int*,int);
+bool isTransitive(int*,int);
+bool isAsymmetric(int*,int);
+
+int main() {
+    int n, elem_count;
+    int *elements=NULL,*relation=NULL;
+
+    printf("Enter the number of elements in the set: ");
+    scanf("%d", &elem_count);
+    elements=(int*)malloc(elem_count*sizeof(int));
+    printf("Enter the elements of the set: ");
+    for (int i = 0; i < elem_count; i++){
+        scanf("%d", elements+i);
+    }
+
+    printf("Enter the number of pairs in the relation: ");
+    scanf("%d", &n);
+    relation=(int*)malloc(n*2*sizeof(int));
+    printf("Enter the pairs of the relation (a b):\n");
+    for (int i = 0; i < 2*n; i+=2) {
+        scanf("%d %d", relation+i,relation+i+1);
+    }
+
+
+    // Check properties
+    bool reflexive = isReflexive(relation, n, elements, elem_count);
+    bool irreflexive = isIrreflexive(relation, n, elements, elem_count);
+    bool symmetric = isSymmetric(relation, n);
+    bool asymmetric = isAsymmetric(relation, n);
+    bool transitive = isTransitive(relation, n);
+    bool antisymmetric = isAntisymmetric(relation, n);
+
+    if (reflexive) {
+        printf("The relation is Reflexive.\n");
+    } 
+    else {
+        printf("The relation is NOT Reflexive.\n");
+    }
+
+    if(irreflexive){
+        printf("The relation is Irreflexive.\n");
+    } 
+    else {
+        printf("The relation is NOT Irreflexive.\n");
+    }
+    if(symmetric){
+        printf("The relation is Symmetric.\n");
+    } 
+    else{
+        printf("The relation is NOT Symmetric.\n");
+    }
+    if(asymmetric){
+        printf("The relation is Asymmetric.\n");
+    } 
+    else{
+        printf("The relation is NOT Asymmetric.\n");
+    }
+    if(antisymmetric){
+        printf("The relation is Antisymmetric.\n");
+    } 
+    else{
+        printf("The relation is NOT Antisymmetric.\n");
+    }
+    if (transitive){
+        printf("The relation is Transitive.\n");
+    }else{
+        printf("The relation is NOT Transitive.\n");
+    }
+    // Check equivalence relation
+    if (reflexive && symmetric && transitive) {
+        printf("The relation is an Equivalence Relation.\n");
+    } 
+    else{
+        printf("The relation is NOT an Equivalence Relation.\n");
+    }
+    return 0;
+}
 
 // Function to check reflexivity
-bool isReflexive(int relation[][2], int n, int* elements, int elem_count) {
-    for (int i = 0; i < elem_count; i++) {
+bool isReflexive(int* relation, int n, int* elements, int elem_count) {
+    for (int i = 0; i < elem_count; i++){
         int found = 0;
-        for (int j = 0; j < n; j++) {
-            if (*(elements+i) == relation[j][0] && *(elements+i) == relation[j][1]) {
+        for (int j = 0; j < 2*n; j+=2){
+            if (*(elements+i) == *(relation+j) && *(elements+i) == *(relation+j+1)){
                 found = 1;
                 break;
             }
@@ -20,12 +100,12 @@ bool isReflexive(int relation[][2], int n, int* elements, int elem_count) {
 }
 
 //Function to check irreflexive
-bool isIrreflexive(int relation[][2],int n,int* elements,int elem_count){
+bool isIrreflexive(int* relation,int n,int* elements,int elem_count){
     for(int i=0;i<elem_count;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<2*n;j+=2)
         {
-            if(*(elements+i) == relation[j][0] && *(elements+i) == relation[j][1])
+            if(*(elements+i) == *(relation+j) && *(elements+i) == *(relation+j+1))
             {
                 return false;
             }
@@ -33,15 +113,14 @@ bool isIrreflexive(int relation[][2],int n,int* elements,int elem_count){
     }
     return true;
 }
-
-// Function to check symmetry
-bool isSymmetric(int relation[][2], int n) {
-    for (int i = 0; i < n; i++) {
-        int a = relation[i][0], b = relation[i][1];
-        if (a != b) {
+//Function to check symmetric
+bool isSymmetric(int* relation,int n) {
+    for (int i = 0; i < 2*n; i+=2){
+        int a = *(relation+i), b = *(relation+i+1);
+        if (a != b){
             int found = 0;
-            for (int j = 0; j < n; j++) {
-                if (relation[j][0] == b && relation[j][1] == a) {
+            for (int j = 0; j < 2*n; j+=2) {
+                if (*(relation+j) == b && *(relation+j+1) == a) {
                     found = 1;
                     break;
                 }
@@ -53,34 +132,28 @@ bool isSymmetric(int relation[][2], int n) {
 }
 
 //Function to check antisymmetric
-bool isAntisymmetric(int relation[][2],int n)
-{
-for(int i=0;i<n;i++)
-{
-    int a = relation[i][0],b = relation[i][1];
-    if(a!=b)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(relation[j][0] == b && relation[j][1] == a)
-            {
-                return false;
+bool isAntisymmetric(int* relation,int n){
+    for(int i=0;i<n;i++){
+        int a = *(relation+i), b = *(relation+i+1);
+        if(a!=b){
+            for(int j=0;j<2*n;j+=2){
+                if(*(relation+j) == b && *(relation+j+1) == a){
+                    return false;
+                }
             }
         }
     }
-}
-return true;
+    return true;
 }
 
-// Function to check transitivity
-bool isTransitive(int relation[][2], int n) {
+bool isTransitive(int *relation, int n) {
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (relation[i][1] == relation[j][0]) {
-                int a = relation[i][0], c = relation[j][1];
+        for (int j = 0; j < 2*n; j+=2) {
+            if (*(relation+j) == *(relation+j+i)){
+                int a = *(relation+i), c = *(relation+i+1);
                 int found = 0;
-                for (int k = 0; k < n; k++) {
-                    if (relation[k][0] == a && relation[k][1] == c) {
+                for (int k = 0; k < 2*n; k+=2) {
+                    if (*(relation+k) == a && *(relation+k+1) == c) {
                         found = 1;
                         break;
                     }
@@ -92,92 +165,19 @@ bool isTransitive(int relation[][2], int n) {
     return true;
 }
 
-bool isAsymmetric(int relation[][2], int n) {
+bool isAsymmetric(int *relation, int n) {
     for (int i = 0; i < n; i++) {
-        int a = relation[i][0], b = relation[i][1];
+        int a = *(relation+i), b = *(relation+i+1);
         if (a == b) {
             // A relation is not asymmetric if (a, a) exists
             return false;
         }
-        for (int j = 0; j < n; j++) {
-            if (relation[j][0] == b && relation[j][1] == a) {
+        for (int j = 0; j < 2*n; j+=2) {
+            if (*(relation+j) == b && *(relation+j+1) == a) {
                 // A relation is not asymmetric if both (a, b) and (b, a) exist
                 return false;
             }
         }
     }
     return true;
-}
-
-int main() {
-    int n, elem_count, relation[MAX][2];
-    int *elements=NULL,*relation;
-    printf("Enter the number of elements in the set: ");
-    scanf("%d", &elem_count);
-    elements=(int*)malloc(elem_count*sizeof(int));
-    printf("Enter the elements of the set: ");
-    for (int i = 0; i < elem_count; i++) {
-        scanf("%d", elements+i);
-    }
-
-    printf("Enter the number of pairs in the relation: ");
-    scanf("%d", &n);
-
-    printf("Enter the pairs of the relation (a b):\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d %d", &relation[i][0], &relation[i][1]);
-    }
-
-
-// Check properties
-    bool reflexive = isReflexive(relation, n, elements, elem_count);
-    bool irreflexive = isIrreflexive(relation, n, elements, elem_count);
-    bool symmetric = isSymmetric(relation, n);
-    bool asymmetric = isAsymmetric(relation, n);
-    bool transitive = isTransitive(relation, n);
-    bool antisymmetric = isAntisymmetric(relation, n);
-
-    if (reflexive) {
-        printf("The relation is Reflexive.\n");
-    } else {
-        printf("The relation is NOT Reflexive.\n");
-    }
-
-    if (irreflexive) {
-        printf("The relation is Irreflexive.\n");
-    } else {
-        printf("The relation is NOT Irreflexive.\n");
-    }
-
-    if (symmetric) {
-        printf("The relation is Symmetric.\n");
-    } else {
-        printf("The relation is NOT Symmetric.\n");
-    }
-    if (asymmetric) {
-        printf("The relation is Asymmetric.\n");
-    } else {
-        printf("The relation is NOT Asymmetric.\n");
-    }
-    if (antisymmetric) {
-        printf("The relation is Antisymmetric.\n");
-    } else {
-        printf("The relation is NOT Antisymmetric.\n");
-    }
-
-    if (transitive) {
-        printf("The relation is Transitive.\n");
-    } else {
-        printf("The relation is NOT Transitive.\n");
-    }
-
-    
-
-    // Check equivalence relation
-    if (reflexive && symmetric && transitive) {
-        printf("The relation is an Equivalence Relation.\n");
-    } else {
-        printf("The relation is NOT an Equivalence Relation.\n");
-    }
-    return 0;
 }
